db33cf78bfb638d1d0d5106cbce771b9a0b12c01
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2014-12-23 22:57:47 -0800

    needs testing

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
-// var Keen = require('keen.io');
+var Keen = require('keen.io');
+
+// Configuration
+var config = require('./config.json');
+var pollPeriod = 500;
+var present = false;
+var countdown;
+var count = 0;
+
+// Set up hardware
 var tessel = require('tessel');
 var rfid = require('rfid-pn532').use(tessel.port['A']);
 
-var pollPeriod = 500;
-var authorized = false;
-var countdown;
+// Set up event streaming
+console.log("Setting up Keen...");
+var keen = Keen.configure({
+  projectId: config.projectId,
+  writeKey: config.writeKey,
+  readKey: config.readKey
+});
 
+// When RFID reader is ready to read cards
 rfid.on('ready', function () {
   console.log('RFID reader ready and waiting.');
   rfid.setPollPeriod(pollPeriod);
 });
 
+// When RFID card sensed
 rfid.on('data', function (data) {
   console.log(data.uid);
   clearTimeout(countdown);
-  authorized = true;
-  console.log('authorized:', authorized);
+  if (!present) {
+    present = true;
+    console.log('card present:', present);
+    sendData(data.uid, present);
+  }
+  // Make sure card is still there
   countdown = setTimeout(function () {
-    authorized = false;
-    console.log('authorized:', authorized);
+    present = false;
+    console.log('card present:', present);
+    sendData(data.uid, present);
   }, pollPeriod + 500);
 });
 
-
-
-// console.log("Setting up Keen...");
-// var keen = Keen.configure({
-//   projectId: "5499e47dd2eaaa02d8b3fcb6",
-//   writeKey: "0cc3835d31febd0a0529d85be44f97cd7dcaabc24a7296925d91ca39b1eb04e76b99f7dba8bad3b6edc74099f1d81cded4ecf6fcf7ec634e8418557589b6ab894b8f4baea24735483168769e9185d9384b81e0a704fae587b6848ae7157ac2c7c2b47095c149b77076d8a9f1e31abca4",
-//   readKey: "4011e80b28e4e0962fc0672a1f165a200d189a7ae12a165f8f9c2809ae8596bfcd7ccab4fcba569f32160675a7940658be34f4cfea8992f610c13154fb680f706e0323811c1b3b037a1db68c4bd06e9fdec5ac9d50bdff03d9c62c45e85e7946502079284c38d694b375bb82b21ea5f9"
-// });
-//
-// // every second send up all the accelerometer data
-// setTimeout(function sendData(){
-//   keen.addEvent("accel", {data: dataArray}, function(err){
-//     if (err) throw err;
-//
-//     console.log("Added event #"+count, "data: ", dataArray);
-//     count++;
-//     dataArray = [];
-//     setTimeout(sendData, 1000);
-//   });
-// }, 1000);
+// Sending data to Keen
+function sendData (data) {
+  keen.addEvent('rfid', {data: data}, function () {
+    console.log("Added event #" + count, "data: ", data);
+    count++;
+  });
+}
